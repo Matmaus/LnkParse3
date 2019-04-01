cbcd6588025643d101ed0276843b8f3e3d99c472
Matúš Jasnický <matus.jasnicky@avast.com>
2019-04-01 22:43:03 +0200

    Refactored code to allow create pip package.

--- setup.py
+++ setup.py
@@ -4,12 +4,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-setup(name='lnkfile',
-      version='0.2.1',
-      description='Windows Shortcut file (LNK) parser',
-      url='https://github.com/silascutler/LnkParse',
-      author='Silas Cutler',
-      author_email='silas.cutler@gmail.com',
-      license='MIT',
-      packages=['lnkfile'],
-      zip_safe=False)
+setup(
+	name='LnkParse3',
+    version='0.3.0',
+    description='Windows Shortcut file (LNK) parser',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Matmaus/LnkParse',
+    author='Matúš Jasnický',
+    author_email='matusjas.work@gmail.com',
+    license='MIT',
+    packages=['LnkParse3'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
