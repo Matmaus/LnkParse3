6d6c4145fa1b8de6786c87ae5914323b34ac2879
Matmaus <matusjas.work@gmail.com>
2021-03-16 10:11:44 +0100

    Bump version: 1.0.0 -> 1.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='LnkParse3',
-    version='1.0.0',
+    version='1.1.0',
     description='Windows Shortcut file (LNK) parser',
     long_description=long_description,
     long_description_content_type="text/markdown",
