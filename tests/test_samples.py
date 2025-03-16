2da2ecd4eda8d9231a77801a637ade38c2a22ee3
Matmaus <matusjas.work@gmail.com>
2025-03-16 13:47:07 +0100

    Encode test samples in base64 to avoid execution

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -1,8 +1,9 @@
+import base64
 import json
 import os
 import unittest
 import warnings
-from contextlib import redirect_stdout
+from contextlib import contextmanager, redirect_stdout
 from io import StringIO
 
 import LnkParse3
@@ -10,6 +11,10 @@ import LnkParse3
 TARGET_DIR = os.path.join(os.path.dirname(__file__), 'samples')
 JSON_DIR = os.path.join(os.path.dirname(__file__), 'json')
 
+@contextmanager
+def open_sample(path):
+    with open(path, 'rb') as indata:
+        yield base64.b64decode(indata.read())
 
 class TestSamples(unittest.TestCase):
     def setUp(self):
@@ -19,8 +24,8 @@ class TestSamples(unittest.TestCase):
     def test_json_print_for_all_samples(self):
         for entry in os.scandir(TARGET_DIR):
             with self.subTest(msg=entry.name):
-                with open(entry.path, 'rb') as indata:
-                    lnk = LnkParse3.lnk_file(indata)
+                with open_sample(entry.path) as indata:
+                    lnk = LnkParse3.lnk_file(indata=indata)
 
                 mock_stdout = StringIO()
                 with redirect_stdout(mock_stdout):
@@ -35,8 +40,8 @@ class TestSamples(unittest.TestCase):
                 self.assertDictEqual(our, their, msg=f'failed on test file {entry.name!r}')
 
     def test_unwanted_attributes_are_not_printed_if_not_specified(self):
-        with open('tests/samples/microsoft_example', 'rb') as indata:
-            lnk = LnkParse3.lnk_file(indata)
+        with open_sample('tests/samples/microsoft_example') as indata:
+            lnk = LnkParse3.lnk_file(indata=indata)
 
         mock_stdout = StringIO()
         with redirect_stdout(mock_stdout):
@@ -51,8 +56,8 @@ class TestSamples(unittest.TestCase):
         self.assertDictEqual(our, their)
 
     def test_readable_with_network_info(self):
-        with open('tests/samples/network_info', 'rb') as indata:
-            lnk = LnkParse3.lnk_file(indata)
+        with open_sample('tests/samples/network_info') as indata:
+            lnk = LnkParse3.lnk_file(indata=indata)
 
         mock_stdout = StringIO()
         with redirect_stdout(mock_stdout):
@@ -67,8 +72,8 @@ class TestSamples(unittest.TestCase):
         self.assertEqual(our, their)
 
     def test_readable_with_local_info(self):
-        with open('tests/samples/sample', 'rb') as indata:
-            lnk = LnkParse3.lnk_file(indata)
+        with open_sample('tests/samples/sample') as indata:
+            lnk = LnkParse3.lnk_file(indata=indata)
 
         mock_stdout = StringIO()
         with redirect_stdout(mock_stdout):
@@ -83,8 +88,8 @@ class TestSamples(unittest.TestCase):
         self.assertEqual(our, their)
 
     def test_print_shortcut_target_json(self):
-        with open('tests/samples/microsoft_example', 'rb') as indata:
-            lnk = LnkParse3.lnk_file(indata)
+        with open_sample('tests/samples/microsoft_example') as indata:
+            lnk = LnkParse3.lnk_file(indata=indata)
 
         mock_stdout = StringIO()
         with redirect_stdout(mock_stdout):
@@ -99,8 +104,8 @@ class TestSamples(unittest.TestCase):
         self.assertDictEqual(our, their)
 
     def test_print_shortcut_target_readable(self):
-        with open('tests/samples/microsoft_example', 'rb') as indata:
-            lnk = LnkParse3.lnk_file(indata)
+        with open_sample('tests/samples/microsoft_example') as indata:
+            lnk = LnkParse3.lnk_file(indata=indata)
 
         mock_stdout = StringIO()
         with redirect_stdout(mock_stdout):
@@ -115,8 +120,8 @@ class TestSamples(unittest.TestCase):
         self.assertEqual(our, their)
 
     def test_print_unknown_target(self):
-        with open('tests/samples/unknown_target', 'rb') as indata:
-            lnk = LnkParse3.lnk_file(indata)
+        with open_sample('tests/samples/unknown_target') as indata:
+            lnk = LnkParse3.lnk_file(indata=indata)
 
         mock_stdout = StringIO()
         with redirect_stdout(mock_stdout):
@@ -131,8 +136,8 @@ class TestSamples(unittest.TestCase):
         self.assertEqual(our, their)
 
     def test_print_unknown_block(self):
-        with open('tests/samples/unknown_block', 'rb') as indata:
-            lnk = LnkParse3.lnk_file(indata)
+        with open_sample('tests/samples/unknown_block') as indata:
+            lnk = LnkParse3.lnk_file(indata=indata)
 
         mock_stdout = StringIO()
         with redirect_stdout(mock_stdout):
