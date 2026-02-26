214cbaa6f77efdbbcfa7bf67d097e5de74ca88e9
Matmaus <matusjas.work@gmail.com>
2026-02-26 19:49:17 +0100

    Implement ControlPanel target class

--- LnkParse3/target/control_panel.py
+++ LnkParse3/target/control_panel.py
@@ -2,9 +2,23 @@ from LnkParse3.decorators import uuid
 from LnkParse3.target.lnk_target_base import LnkTargetBase
 
 
+"""
+----------------------------------------------------------------------
+|              0-7b              |               8-15b               |
+----------------------------------------------------------------------
+| ClassTypeIndicator == 0x70/71 |             Unknown                |
+----------------------------------------------------------------------
+|                           Unknown                                  |
+|                             12 B                                   |
+----------------------------------------------------------------------
+|              <GUID> ControlPanelItemIdentifier                     |
+|                             16 B                                   |
+----------------------------------------------------------------------
+"""
+
+
 # https://github.com/libyal/libfwsi/blob/master/documentation/Windows%20Shell%20Item%20format.asciidoc#38-control-panel-shell-item
 class ControlPanel(LnkTargetBase):
-    # TODO Not implemented
     def __init__(self, *args, **kwargs):
         self.name = "Control panel"
         return super().__init__(*args, **kwargs)
